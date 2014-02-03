c27d029b9fed2184d75801842bcce0bd1bd1e303
sijakubo <sijakubo@gmail.com>
2014-02-03 13:58:21 +0100

    simulation in ein unterverzeichnis gezogen

--- simulation/src/main/java/uni/oldenburg/client/view/LoginView.java
+++ simulation/src/main/java/uni/oldenburg/client/view/LoginView.java
@@ -3,8 +3,6 @@ package uni.oldenburg.client.view;
 
 
 
-import java.util.ArrayList;
-import java.util.List;
 import uni.oldenburg.client.presenter.LoginPresenter;
 import com.google.gwt.user.client.ui.Composite;
 
