9682e1d5a2839f8a6eb1409ca0311cc8f6b6430f
CSchwa <christopher.schwarz@uni-oldenburg.de>
2014-02-25 15:36:32 +0100

    Menüzeile

--- simulation/src/main/java/uni/oldenburg/client/view/MainFrameView.java
+++ simulation/src/main/java/uni/oldenburg/client/view/MainFrameView.java
@@ -9,38 +9,53 @@ import com.google.gwt.user.client.ui.VerticalPanel;
 
 public class MainFrameView extends Composite implements MainFramePresenter.IDisplay {
 	
-	private MenuBar menuBar;
+	private MenuBar  menuBar;
 	
-	private MenuBar fileMenu;
-	private MenuBar editMenu;
-	private MenuBar viewMenu;
+	private MenuBar  fileMenuBar;
+	private MenuBar  editMenuBar;
+	private MenuBar  viewMenuBar;
 	
 	public MainFrameView() {
 
-		VerticalPanel vpLogin = new VerticalPanel();
+		VerticalPanel vpMainFrame = new VerticalPanel();
 			
 		//file menu
 		
-		fileMenu = new MenuBar(true);
-		fileMenu.setAnimationEnabled(true);
-		
-		fileMenu.addItem("New", new Command() {
+		fileMenuBar = new MenuBar(true);
+		fileMenuBar.setAnimationEnabled(true);		
+		fileMenuBar.addItem("Load Scenario", new Command() {
 			public void execute() {
 				
 			}
 		});
-		fileMenu.addItem("Open", new Command() {
+		fileMenuBar.addItem("Recently Used", new Command() {
 			public void execute() {
 				
 			}
 		});
-		fileMenu.addItem("Save", new Command() {
+		fileMenuBar.addSeparator();
+		fileMenuBar.addItem("Adjust Scenario", new Command() {
 			public void execute() {
 				
 			}
 		});
-		fileMenu.addSeparator();
-		fileMenu.addItem("Exit", new Command() {
+		fileMenuBar.addItem("Save", new Command() {
+			public void execute() {
+				
+			}
+		});
+		fileMenuBar.addItem("Save As", new Command() {
+			public void execute() {
+				
+			}
+		});
+		fileMenuBar.addSeparator();
+		fileMenuBar.addItem("Print", new Command() {
+			public void execute() {
+				
+			}
+		});
+		fileMenuBar.addItem("Exit", new Command() {
 			public void execute() {
 				
 			}
@@ -48,24 +63,99 @@ public class MainFrameView extends Composite implements MainFramePresenter.IDisp
 
 		//edit menu
 		
-		editMenu = new MenuBar(true);
-		editMenu.setAnimationEnabled(true);
+		editMenuBar = new MenuBar(true);
+		editMenuBar.setAnimationEnabled(true);
+		editMenuBar.addItem("Undo", new Command() {
+			public void execute() {
+				
+			}
+		});
+		editMenuBar.addItem("Redo", new Command() {
+			public void execute() {
+				
+			}
+		});
+		editMenuBar.addSeparator();
+		editMenuBar.addItem("Define Job Set", new Command() {
+			public void execute() {
+				
+			}
+		});
+		editMenuBar.addItem("Edit Jobs", new Command() {
+			public void execute() {
+				
+			}
+		});
+		editMenuBar.addItem("Change Database", new Command() {
+			public void execute() {
+				
+			}
+		});
+		editMenuBar.addItem("Simulation Settings", new Command() {
+			public void execute() {
+				
+			}
+		});
+		editMenuBar.addItem("Start/Stop Simulation", new Command() {
+			public void execute() {
+				
+			}
+		});
+		editMenuBar.addSeparator();
+		editMenuBar.addItem("Save As Image", new Command() {
+			public void execute() {
+				
+			}
+		});
+		editMenuBar.addItem("PlugIn", new Command() {
+			public void execute() {
+				
+			}
+		});
 
 		//view menu
 		
-		viewMenu = new MenuBar(true);
-		viewMenu.setAnimationEnabled(true);
+		viewMenuBar = new MenuBar(true);
+		viewMenuBar.setAnimationEnabled(true);
+		viewMenuBar.addItem("Brightness", new Command() {
+			public void execute() {
+				
+			}
+		});
+		viewMenuBar.addSeparator();
+		viewMenuBar.addItem("Zoom In", new Command() {
+			public void execute() {
+				
+			}
+		});
+		viewMenuBar.addItem("Zoom Out", new Command() {
+			public void execute() {
+				
+			}
+		});
+		viewMenuBar.addSeparator();
+		viewMenuBar.addItem("Fullscreen", new Command() {
+			public void execute() {
+				
+			}
+		});
 		
 		//menu bar
 		
 		menuBar = new MenuBar();
-		menuBar.addItem("File", fileMenu);
+		menuBar.addItem("File", fileMenuBar);
 		menuBar.addSeparator();
-		menuBar.addItem("Edit", editMenu);
+		menuBar.addItem("Edit", editMenuBar);
 		menuBar.addSeparator();
-		menuBar.addItem("View", viewMenu);
-		vpLogin.add(menuBar);
+		menuBar.addItem("View", viewMenuBar);
+		menuBar.addSeparator();
+		menuBar.addItem("Help", new Command() {
+			public void execute() {
+				
+			}
+		});
+		vpMainFrame.add(menuBar);
 		
-		initWidget(vpLogin);
+		initWidget(vpMainFrame);
 	}
 }
