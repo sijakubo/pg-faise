4271ae6d500560dfd24993be5c2c24f7932d9a2d
CSchwa <christopher.schwarz@uni-oldenburg.de>
2014-02-26 13:51:02 +0100

    Auftragsliste und zwei Buttons zur MainFrameView hinzugefügt.

--- simulation/src/main/java/uni/oldenburg/client/view/MainFrameView.java
+++ simulation/src/main/java/uni/oldenburg/client/view/MainFrameView.java
@@ -1,24 +1,51 @@
 package uni.oldenburg.client.view;
 
+import java.util.Arrays;
+import java.util.List;
+
 import uni.oldenburg.client.presenter.MainFramePresenter;
 
+import com.google.gwt.canvas.client.Canvas;
+import com.google.gwt.user.cellview.client.CellTable;
+import com.google.gwt.user.cellview.client.SimplePager;
+import com.google.gwt.user.cellview.client.TextColumn;
 import com.google.gwt.user.client.Command;
-import com.google.gwt.user.client.ui.MenuBar;
+import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.MenuBar;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.ToggleButton;
 import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.view.client.AsyncDataProvider;
+import com.google.gwt.view.client.HasData;
 
 public class MainFrameView extends Composite implements MainFramePresenter.IDisplay {
 	
-	private MenuBar  menuBar;
+	private MenuBar        menuBar;	
+	private MenuBar        fileMenuBar;
+	private MenuBar        editMenuBar;
+	private MenuBar        viewMenuBar;
 	
-	private MenuBar  fileMenuBar;
-	private MenuBar  editMenuBar;
-	private MenuBar  viewMenuBar;
+	private CellTable<Job> jobTable = new CellTable<Job>();
+	private Button         btStrategies;
+	private ToggleButton   tbVirtualHybridSwitch;
+	
+	private Canvas       canvas;
+	
+	private static final List<Job> JOBS = Arrays.asList(
+		      new Job(1, "XY"),
+		      new Job(2, "zt"),
+		      new Job(3, "Patata"),
+		      new Job(4, "UO"));
 	
 	public MainFrameView() {
 
-		VerticalPanel vpMainFrame = new VerticalPanel();
+		VerticalPanel   vpMainFrame = new VerticalPanel();
+		HorizontalPanel hpSubFrame = new HorizontalPanel();
+		VerticalPanel   vpLeftFrame = new VerticalPanel();
 			
+		//--- menu bar ---
+		
 		//file menu
 		
 		fileMenuBar = new MenuBar(true);
@@ -154,8 +181,78 @@ public class MainFrameView extends Composite implements MainFramePresenter.IDisp
 				
 			}
 		});
-		vpMainFrame.add(menuBar);
 		
+		//--- left panel ---
+
+		//table
+		
+	    jobTable.setPageSize(5);
+	    TextColumn<Job> jobNumberColumn = new TextColumn<Job>() {
+	    	@Override
+	        public String getValue(Job object) {
+	    		return "" + object.jobNumber;
+	        }
+	    };
+	    jobTable.addColumn(jobNumberColumn, "Job Number");	    
+	    TextColumn<Job> jobColumn = new TextColumn<Job>() {
+	    	@Override
+	        public String getValue(Job object) {
+	    		return object.job;
+	        }
+	    };
+	    jobTable.addColumn(jobColumn, "Job");
+	    vpLeftFrame.add(jobTable);
+
+	    AsyncDataProvider<Job> provider = new AsyncDataProvider<Job>() {
+	    	@Override
+	        protected void onRangeChanged(HasData<Job> display) {
+	    		int start = display.getVisibleRange().getStart();
+	    		int end = start + display.getVisibleRange().getLength();
+	    		end = end >= JOBS.size() ? JOBS.size() : end;
+	    		List<Job> sub = JOBS.subList(start, end);
+	    		updateRowData(start, sub);
+	    	}
+	    };
+	    provider.addDataDisplay(jobTable);
+	    provider.updateRowCount(JOBS.size(), true);
+	    
+	    SimplePager pager = new SimplePager();
+	    pager.setDisplay(jobTable);
+
+	    VerticalPanel vp = new VerticalPanel();
+	    vp.add(jobTable);
+	    vp.add(pager);
+	    vpLeftFrame.add(vp);
+	    
+	    //togglebutton for switching between hybrid and virtual mode
+	    
+		tbVirtualHybridSwitch = new ToggleButton("Virtual / Hybrid");
+		tbVirtualHybridSwitch.setText("Virtual / Hybrid");
+		vpLeftFrame.add(tbVirtualHybridSwitch);
+		
+		//button for changing pathfinding strategy
+		
+		btStrategies = new Button("Strategies");
+		btStrategies.setText("Strategies");
+		vpLeftFrame.add(btStrategies);
+		
+		hpSubFrame.add(vpLeftFrame);
+		
+		canvas = Canvas.createIfSupported();
+		hpSubFrame.add(canvas);
+		
+		vpMainFrame.add(menuBar);
+		vpMainFrame.add(hpSubFrame);
 		initWidget(vpMainFrame);
+	}
+	
+	private static class Job {
+	    private final int jobNumber;
+	    private final String job;
+
+	    public Job(int jobNumber, String job) {
+	    	this.jobNumber = jobNumber;
+	      	this.job = job;
+	    }
 	}
 }
