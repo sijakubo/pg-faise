fcf1bb6e989f41caa8d879c69bd163c292b16448
JGM <jangerd.mess@googlemail.com>
2014-02-28 18:34:02 +0100

    Initialer Commit des UART-Drivers

---
+++ flow/drivers/uart_drv.c
@@ -0,0 +1,15 @@
+/*
+ * uart_drv.c
+ *
+ * Created: 26.02.2014 13:01:47
+ *  Author: JanGerd
+ */ 
+#include "drivers/uart_drv.h"
+
+void usart0_send(uint8_t *buf, uint8_t size)
+{
+	while(size > 0) {
+		putchar(*buf++);
+		size--;
+	}
+}
