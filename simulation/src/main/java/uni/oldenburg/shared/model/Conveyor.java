83867b29823de3f9e194d86fd524b007d631df0c
sijakubo <sijakubo@gmail.com>
2014-03-06 22:00:39 +0100

    Server Fehler 500 Canvas zunächst als Transient innerhalb ders Conveyors markiert

--- simulation/src/main/java/uni/oldenburg/shared/model/Conveyor.java
+++ simulation/src/main/java/uni/oldenburg/shared/model/Conveyor.java
@@ -8,77 +8,80 @@ import com.google.gwt.dom.client.CanvasElement;
 @SuppressWarnings("serial")
 public abstract class Conveyor implements Serializable {
 	public static final String TABLE_NAME = "szenario_conveyor";
-	
+
 	private int ID;
-	
+
 	private int x;
 	private int y;
 	private int width;
 	private int height;
-	
-	private Canvas canvas;	
+
+	private transient Canvas canvas;
 	protected String strType;
-	
+
 	private static int ID_Counter = 0;
-	
-	public abstract String getType();	
+
+	public abstract String getType();
 	protected abstract Canvas createCanvas(Canvas canvas);
-	
-	protected Conveyor(int x, int y, int width, int height) {
+
+    protected Conveyor() {
+    }
+
+    protected Conveyor(int x, int y, int width, int height) {
 		ID = ++ID_Counter;
 		strType = getType();
 
 		setPosition(x, y);
 		this.width = width;
 		this.height = height;
-		
+
 		canvas = null;
 	}
-	
+
 	public int getID() {
 		return ID;
 	}
-	
+
 	public void setPosition(int x, int y) {
 		this.x = x;
 		this.y = y;
 	}
-	
+
 	public int getX() {
 		return this.x;
 	}
-	
+
 	public int getY() {
 		return this.y;
 	}
-	
+
 	public int getWidth() {
 		return this.width;
 	}
-	
+
 	public int getHeight() {
 		return this.height;
-	}	
-	
+	}
+
 	public void move(int x_rel, int y_rel) {
 		this.x += x_rel;
 		this.y += y_rel;
 	}
-	
+
 	public CanvasElement getCanvasElement() {
 		if (canvas == null) {
 			canvas = Canvas.createIfSupported();
-			
+
 			if (canvas != null) {
 				canvas.setWidth(width + "px");
 				canvas.setHeight(height + "px");
 				canvas.setCoordinateSpaceWidth(width);
 				canvas.setCoordinateSpaceHeight(height);
-				
-				canvas = createCanvas(canvas);	
+
+				canvas = createCanvas(canvas);
 			}
 		}
-		
+
 		return canvas.getCanvasElement();
 	}
 }
