20e9f4faece5c2429f613674031b1bf25c84a351
JGM <jangerd.mess@googlemail.com>
2014-03-26 09:43:31 +0100

    initialer Commit des extflash_service

---
+++ flow/service/extflash_service.c
@@ -0,0 +1,40 @@
+/*
+ * extflash_service.c
+ *
+ * Created: 26.03.2014 07:36:13
+ *  Author: JanGerd
+ */ 
+
+#include "extflash_service.h"
+
+void extflash_write_page_to_buffer1(uint16_t page){
+	
+}
+
+void extflash_write_page_to_buffer2(uint16_t page){
+	
+}
+
+void extflash_write_buffer1(uint16_t address, uint8_t len, uint8_t* data){
+	
+}
+
+void extflash_write_buffer2(uint16_t address, uint8_t len, uint8_t* data){
+	
+}
+
+uint8_t extflash_compare_buffer1_to_mem(uint16_t page){
+	return 0;
+}
+
+uint8_t extflash_compare_buffer2_to_mem(uint16_t page){
+	return 0;
+}
+
+void extflash_write_buffer1_to_mem(uint16_t page){
+	
+}
+
+void extflash_write_buffer2_to_mem(uint16_t page){
+	
+}
---
+++ flow/service/extflash_service.h
@@ -0,0 +1,25 @@
+/*
+ * extflash_service.h
+ *
+ * Created: 26.03.2014 07:36:42
+ *  Author: JanGerd
+ */ 
+
+
+#ifndef EXTFLASH_SERVICE_H_
+#define EXTFLASH_SERVICE_H_
+
+
+void extflash_write_page_to_buffer1(uint16_t page);
+void extflash_write_page_to_buffer2(uint16_t page);
+void extflash_write_buffer1(uint16_t address, uint8_t len, uint8_t* data);
+void extflash_write_buffer2(uint16_t address, uint8_t len, uint8_t* data);
+
+uint8_t extflash_compare_buffer1_to_mem(uint16_t page);
+uint8_t extflash_compare_buffer2_to_mem(uint16_t page);
+
+void extflash_write_buffer1_to_mem(uint16_t page);
+void extflash_write_buffer2_to_mem(uint16_t page);
+
+
+#endif /* EXTFLASH_SERVICE_H_ */
