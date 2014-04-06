a14d215993d3cc2626b9df87ec23b6fdc93e1c2b
JGM <jangerd.mess@googlemail.com>
2014-04-06 10:55:26 +0200

    - Entfernen des uart_service: Überflüssig.

--- flow/service/com_service.c
+++ flow/service/com_service.c
@@ -7,6 +7,8 @@
 
 #include "service/com_service.h"
 
+extern struct broadcast_conn broadcast;
+
 void com_receive(uint8_t* msg){
 	if (msg[0]==COM_CMD_RECEIVE_PACKAGE)
 	{
@@ -19,9 +21,41 @@ void com_receive(uint8_t* msg){
 	else if (msg[0]==COM_CMD_RET_COST)
 	{
 	}
+	if(msg[0] == UART_LED_GREEN_COMMAND){
+		if(msg[1])
+		leds_on(LEDS_GREEN);
+		else
+		leds_off(LEDS_GREEN);
+	}
+	else if(msg[0] == UART_LED_RED_COMMAND){
+		if(msg[1])
+			leds_on(LEDS_RED);
+		else
+			leds_off(LEDS_RED);
+	} else if(msg[0] == UART_RAMP_NUM_PACKAGES){
+		packetbuf_copyfrom(msg, 1);
+		broadcast_send(&broadcast);
+		printf("Asking for number of packages...\n");
+	} else if(msg[0] == UART_RAMP_BAY_STATUS){
+		packetbuf_copyfrom(msg, 2);
+		broadcast_send(&broadcast);
+		printf("Asking for Status of Bay %u...\n", msg[1]);
+	} else if(msg[0] == UART_RAMP_SEPARATE_PACKAGE){
+		packetbuf_copyfrom(msg, 1);
+		broadcast_send(&broadcast);
+		printf("Separating Package...\n");
+	} else if(msg[0] == UART_RAMP_RELEASE_PACKAGE){
+		packetbuf_copyfrom(msg, 1);
+		broadcast_send(&broadcast);
+		printf("Release Package...\n");
+	} else if(msg[0] == UART_RAMP_RELEASE_AND_SEPARATE_PACKAGE){
+		packetbuf_copyfrom(msg, 1);
+		broadcast_send(&broadcast);
+		printf("Release and Separate Package...\n");
+	}
 }
 
-void com_send(unit8_t* msg, uint8_t len){
+void com_send(uint8_t* msg, uint8_t len){
 	if ((msg[0] | 0x7f) == 0x7f)
 	{
 		uart0_send(msg, len);
--- flow/service/com_service.h
+++ flow/service/com_service.h
@@ -11,11 +11,12 @@
 
 #include "contiki.h"
 #include "sys/node-id.h"
-#include "service/uart_service.h"
+#include "dev/leds.h"
+#include "drivers/uart_drv.h"
 
 #define COM_CMD_RECEIVE_PACKAGE 0x50
 #define COM_CMD_DELIVER_PACKAGE 0x55
-#define COM_CMD_RET_COST 0x60
+#define COM_CMD_RET_COST 0x11
 
 #define COM_CMD_DRIVE_IN_RAMP 0x01
 #define COM_CMD_DRIVE_OUT_RAMP 0x02
@@ -23,7 +24,22 @@
 #define COM_CMD_RECEIVE_PACKAGE_ACK 0x20
 #define COM_CMD_DELIVER_PACKAGE_ACK 0x25
 
+//*********************************************
+// Test-Commands for Ramps
+//*********************************************
+#define UART_LED_RED_COMMAND 0x61
+#define UART_LED_GREEN_COMMAND 0x62
+#define UART_RAMP_NUM_PACKAGES 0x63
+#define UART_RAMP_RET_NUM_PACKAGES 0x64
+#define UART_RAMP_BAY_STATUS 0x65
+#define UART_RAMP_RET_BAY_STATUS 0x66
+#define UART_RAMP_RELEASE_AND_SEPARATE_PACKAGE 0x67
+#define UART_RAMP_SEPARATE_PACKAGE 0x68
+#define UART_RAMP_RELEASE_PACKAGE 0x69
+//*********************************************
+
 void com_receive(uint8_t* msg);
+void com_send(uint8_t* msg, uint8_t len);
 
 
 #endif /* COM_SERVICE_H_ */
