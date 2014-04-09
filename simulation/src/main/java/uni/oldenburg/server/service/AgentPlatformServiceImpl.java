8c18a4c0a53543ab370a48dfb86f6aa20a56b046
sijakubo <sijakubo@gmail.com>
2014-04-09 20:26:58 +0200

    FAISE-222: Methode zum hinzufügen eines Agenten erstellt

--- simulation/src/main/java/uni/oldenburg/server/service/AgentPlatformServiceImpl.java
+++ simulation/src/main/java/uni/oldenburg/server/service/AgentPlatformServiceImpl.java
@@ -1,15 +1,18 @@
 package uni.oldenburg.server.service;
 
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
+import jade.core.Agent;
 import jade.core.ProfileImpl;
 import jade.core.Runtime;
 import jade.wrapper.ContainerController;
+import jade.wrapper.StaleProxyException;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import uni.oldenburg.client.service.AgentPlatformService;
 
 public class AgentPlatformServiceImpl extends RemoteServiceServlet implements AgentPlatformService {
-   Logger logger = Logger.getLogger(AgentPlatformServiceImpl.class);
+   private Logger logger = Logger.getLogger(AgentPlatformServiceImpl.class);
+   private ContainerController container;
 
    /**
     * @author sijakubo
@@ -24,7 +27,14 @@ public class AgentPlatformServiceImpl extends RemoteServiceServlet implements Ag
     */
    private void startAgentPlatform() {
       Runtime runtimeInstance = Runtime.instance();
-      ContainerController container = runtimeInstance.createMainContainer(new ProfileImpl());
+      container = runtimeInstance.createMainContainer(new ProfileImpl());
       //container.createNewAgent("admin", "uez2.AdminAgent", null)
+   }
+
+   /**
+    * @author sijakubo
+    */
+   public void addAgentToSimulationPlatform(Agent agent, String nickname) throws StaleProxyException {
+      container.acceptNewAgent(nickname, agent);
    }
 }
