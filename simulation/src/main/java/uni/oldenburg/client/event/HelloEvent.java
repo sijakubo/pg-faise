9690c2dd01844982a54b227e2b72f8e40c1ef378
raalkhat <raschid.alkhatib@uni-oldenburg.de>
2014-04-27 16:09:13 +0200

    Event hinzugefügt

---
+++ simulation/src/main/java/uni/oldenburg/client/event/HelloEvent.java
@@ -0,0 +1,16 @@
+package uni.oldenburg.client.event;
+
+import de.novanic.eventservice.client.event.Event;
+
+
+@SuppressWarnings("serial")
+public class HelloEvent implements Event {
+
+	/**
+	 * Needed for serialization
+	 */
+	public HelloEvent() {
+		
+	}
+
+}
