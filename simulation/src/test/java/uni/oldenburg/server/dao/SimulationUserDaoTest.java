0255095a901be0db4d0ecd562e5939dd69e4d529
Nagihan <nagihan.aydin@uni-oldenburg.de>
2014-04-29 11:43:08 +0200

    Testfälle

--- simulation/src/test/java/uni/oldenburg/server/dao/SimulationUserDaoTest.java
+++ simulation/src/test/java/uni/oldenburg/server/dao/SimulationUserDaoTest.java
@@ -15,4 +15,44 @@ public class SimulationUserDaoTest {
         Assert.assertEquals(user.getName(), "simon");
         Assert.assertNotNull(user.getId());
     }
+    
+    
+    
+    
+    @Test
+    public void testFindUserForID() throws Exception {
+        SimulationUserDao simulationUserDao = new SimulationUserDao();
+        Long l=new Long(19);
+        SimulationUser user=simulationUserDao.findSimulationUserById(l);
+        Assert.assertNotNull(user);
+        Assert.assertEquals(user.getName(), "nagi");
+        
+       
+    }
+    
+    @Test
+    public void testPersistUser() throws Exception {
+    	//The test should secure, that a persisted user is persisted actually
+    	
+    	SimulationUserDao simulationUserDao = new SimulationUserDao();
+    	
+    	//create user
+    	if(simulationUserDao.findUserForUsernameAndPassword("rasch@inf.de", "nagi")==null){
+    		
+    	
+    	SimulationUser user= new SimulationUser("rasch@inf.de", "rasch", "nagi");
+    	
+    	//Persist User
+    	
+    	simulationUserDao.persistSimulationUser(user);
+    	
+    	//Check if the persisted User was successfully persisted
+    	 SimulationUser userTest=simulationUserDao.findUserForUsernameAndPassword("rasch@inf.de", "nagi");
+    	 Assert.assertNotNull(userTest);
+         Assert.assertEquals(userTest.getEmail(),user.getEmail());
+         Assert.assertEquals(userTest.getName(),user.getName());
+         Assert.assertEquals(userTest.getPassword(),user.getPassword());
+    	} 
+    }
 }
+
